5c619138b0fc18a7bd7ac54951f8b74ee06517ba
wujunjie1008 <445404396@qq.com>
2019-09-24 01:25:33 +0800

    1.1
    消除警告，将6阶并入4689阶

--- 031702537/Sudoku/Sudoku/Sudoku.cpp
+++ 031702537/Sudoku/Sudoku/Sudoku.cpp
@@ -8,7 +8,7 @@ int num_list[10][10] = { 0 };
 
 int check(int h,int l,int m) {
 	int num[10] = {0};
-	for (int bh = 0; bh < m; bh++) {		//遍历行
+	for (int bh = 0; bh < m; bh++) {		//遍历行，寻找重复的数字
 		if (num_list[bh][l] != 0) {
 			num[num_list[bh][l]]++;
 			if (num[num_list[bh][l]] > 1)
@@ -18,7 +18,7 @@ int check(int h,int l,int m) {
 	for (int i = 0; i < 10; i++) {
 		num[i] = 0;
 	}
-	for (int bl = 0; bl < m; bl++) {		//遍历列
+	for (int bl = 0; bl < m; bl++) {		//遍历列，寻找重复的数字
 		if (num_list[h][bl] != 0) {
 			num[num_list[h][bl]]++;
 			if (num[num_list[h][bl]] > 1)
@@ -28,27 +28,29 @@ int check(int h,int l,int m) {
 	for (int i = 0; i < 10; i++) {
 		num[i] = 0;
 	}
-	if (m == 4 || m == 8 || m == 9) {
-		int max_h, max_l;								//检验九宫格
-		int gong_h, gong_l;
-		gong_l = (int)sqrt(m);
-		gong_h = (int)(m / gong_l);
-		for (int i = 0; i < m; i++) {
+	if (m == 4 || m == 8 || m == 9 || m == 6) {
+		int max_h = 0, max_l = 0;					//检验九宫格，寻找重复的数字
+		int gong_h = 0, gong_l = 0;
+		if (m == 4 || m == 8 || m == 9) {
+			gong_l = (int)sqrt(m);
+			gong_h = (int)(m / gong_l);
+		}
+		else if (m == 6) {
+			gong_h = (int)sqrt(m);
+			gong_l = (int)(m / gong_h);
+		}
+		for (int i = 1; i < m; i++) {
 			max_h = i * gong_h;
 			if (max_h > h) {
 				break;
 			}
 		}
-		for (int i = 0; i < m; i++) {
+		for (int i = 1; i < m; i++) {
 			max_l = i * gong_l;
 			if (max_l > l) {
 				break;
 			}
 		}
-		if (max_l > 9)
-			max_l = 9;
-		if (max_h > 9)
-			max_h = 9;
 		for (int i = max_h - gong_h; i < max_h; i++) {
 			for (int j = max_l - gong_l; j < max_l; j++) {
 				if (num_list[i][j] != 0) {
@@ -59,36 +61,7 @@ int check(int h,int l,int m) {
 			}
 		}
 	}
-	if (m == 6) {
-		int max_h, max_l;								//检验九宫格
-		int gong_h, gong_l;
-		gong_h = (int)sqrt(m);
-		gong_l = (int)(m / gong_h);
-		for (int i = 0; i < m; i++) {
-			max_h = i * gong_h;
-			if (max_h > h) {
-				break;
-			}
-		}
-		for (int i = 0; i < m; i++) {
-			max_l = i * gong_l;
-			if (max_l > l) {
-				break;
-			}
-		}
-		int i,j;
-		i = max_h - gong_h;
-		j = max_l - gong_l;
-		for (; i < max_h; i++) {
-			for (; j < max_l; j++) {
-				if (num_list[i][j] != 0 && i != h && j != l) {
-					num[num_list[i][j]]++;
-					if (num[num_list[i][j]] > 1)
-						return 0;
-				}
-			}
-		}
-	}
+	
 	return 1;
 }
 
@@ -134,7 +107,6 @@ void count_157(int m) {
 				count++;
 		}
 	}
-	//printf("%d\n", count);
 	for (h = 0; h < m; h++) {
 		for (l = 0; l < m; l++) {
 			if (num_list[h][l] == 0) {
@@ -144,10 +116,8 @@ void count_157(int m) {
 					if (num_list[bh][l] != 0) {
 						if (may_num[num_list[bh][l]] == 0)
 							may_count--;
-						//printf("%d,", may_count);
 						may_num[num_list[bh][l]] = 1;
 					}
-					//printf("%d,", may_count);
 				}
 				for (int bl = 0; bl < m; bl++) {		//遍历列
 					if (num_list[h][bl] != 0) {
@@ -155,17 +125,11 @@ void count_157(int m) {
 							may_count--;
 						may_num[num_list[h][bl]] = 1;
 					}
-					//printf("%d,", may_count);
 				}
 				if (may_count == 1) {					//填写数字
 					for (int i = 1; i <= m; i++) {
 						if (may_num[i] == 0) {
 							num_list[h][l] = i;
-							/*for (int j = 0; j < m; j++) {
-								for (int k = 0; k < m; k++)
-									cout << num_list[j][k];
-								cout << endl;
-							}*/
 							h = 0;
 							l = -1;
 							count--;
@@ -189,7 +153,7 @@ void count_157(int m) {
 	}
 }
 
-void count_489(int m) {
+void count_4689(int m) {
 	cout << "使用唯余法" << endl;
 	int h, l;
 	int may_count;
@@ -223,17 +187,23 @@ void count_489(int m) {
 					}
 					//printf("%d,", may_count);
 				}
-				int max_h, max_l;								//遍历九宫格
-				int gong_h, gong_l;
-				gong_l = (int)sqrt(m);
-				gong_h = (int)(m / gong_l);
-				for (int i = 0; i < m; i++) {
+				int max_h = 0, max_l = 0;								//遍历九宫格
+				int gong_h = 0, gong_l = 0;
+				if (m == 4 || m == 8 || m == 9) {
+					gong_l = (int)sqrt(m);
+					gong_h = (int)(m / gong_l);
+				}
+				else if (m == 6) {
+					gong_h = (int)sqrt(m);
+					gong_l = (int)(m / gong_h);
+				}
+				for (int i = 1; i < m; i++) {
 					max_h = i * gong_h;
 					if (max_h > h) {
 						break;
 					}
 				}
-				for (int i = 0; i < m; i++) {
+				for (int i = 1; i < m; i++) {
 					max_l = i * gong_l;
 					if (max_l > l) {
 						break;
@@ -241,6 +211,14 @@ void count_489(int m) {
 				}
 				for (int i = max_h - gong_h; i < max_h; i++) {
 					for (int j = max_l - gong_l; j < max_l; j++) {
+						if (i > 9)		//消除警告
+							i = 9;
+						if (i < 0)
+							i = 0;
+						if (j > 9)
+							j = 9;
+						if (j < 0)
+							j = 0;
 						if (num_list[i][j] != 0) {
 							if (may_num[num_list[i][j]] == 0)
 								may_count--;
@@ -249,7 +227,6 @@ void count_489(int m) {
 					}
 
 				}
-
 				if (may_count == 1) {					//填写数字
 					for (int i = 1; i <= m; i++) {
 						if (may_num[i] == 0) {
@@ -276,94 +253,6 @@ void count_489(int m) {
 	if (count > 0)
 	{
 		cout << "使用DFS" << endl;
-		DFS(0, m);
-		cout << "成功" << endl;
-	}
-}
-
-void count_6(int m) {
-	cout << "使用唯余法" << endl;
-	int h, l;
-	int may_count;
-	int count = 0;
-	for (h = 0; h < m; h++) {
-		for (l = 0; l < m; l++) {
-			if (num_list[h][l] == 0)
-				count++;
-		}
-	}
-	//printf("%d\n", count);
-	for (h = 0; h < m; h++) {
-		for (l = 0; l < m; l++) {
-			if (num_list[h][l] == 0) {
-				int may_num[10] = { 0 };
-				may_count = m;
-				for (int bh = 0; bh < m; bh++) {		//遍历行
-					if (num_list[bh][l] != 0) {
-						if (may_num[num_list[bh][l]] == 0)
-							may_count--;
-						//printf("%d,", may_count);
-						may_num[num_list[bh][l]] = 1;
-					}
-					//printf("%d,", may_count);
-				}
-				for (int bl = 0; bl < m; bl++) {		//遍历列
-					if (num_list[h][bl] != 0) {
-						if (may_num[num_list[h][bl]] == 0)
-							may_count--;
-						may_num[num_list[h][bl]] = 1;
-					}
-					//printf("%d,", may_count);
-				}
-				int max_h, max_l;								//遍历九宫格
-				int gong_h, gong_l;
-				gong_h = (int)sqrt(m);
-				gong_l = (int)(m / gong_h);
-				for (int i = 0; i < m; i++) {
-					max_h = i * gong_h;
-					if (max_h > h) {
-						break;
-					}
-				}
-				for (int i = 0; i < m; i++) {
-					max_l = i * gong_l;
-					if (max_l > l) {
-						break;
-					}
-				}
-				for (int i = max_h - gong_h; i < max_h; i++) {
-					for (int j = max_l - gong_l; j < max_l; j++) {
-						if (num_list[i][j] != 0) {
-							if (may_num[num_list[i][j]] == 0)
-								may_count--;
-							may_num[num_list[i][j]] = 1;
-						}
-					}
-
-				}
-
-				if (may_count == 1) {					//填写数字
-					for (int i = 1; i <= m; i++) {
-						if (may_num[i] == 0) {
-							num_list[h][l] = i;
-							h = 0;
-							l = -1;
-							count--;
-							printf("%d\n", count);
-							if (count == 0) {
-								printf("成功\n");
-								return;
-							}
-							break;
-						}
-					}
-				}
-			}
-		}
-	}
-	if (count > 0)
-	{
-		cout << "使用DFS"<<endl;
 		DFS(0, m);
 		cout << "成功" << endl;
 	}
@@ -399,11 +288,8 @@ int main(int   argc, char*   argv[])
 	if (m == 1 || m == 5 || m == 7) {
 		count_157(m);
 	}
-	else if (m == 4 || m == 8 || m == 9) {
-		count_489(m);
-	}
-	else if (m == 6) {
-		count_6(m);
+	else if (m == 4 || m == 6 || m == 8 || m == 9) {
+		count_4689(m);
 	}
 	for (h = 0; h < m; h++) {			//文件输出
 		for (l = 0; l < m; l++) {
